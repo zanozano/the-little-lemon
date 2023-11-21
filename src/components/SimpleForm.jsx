9057749bb149bdb6b1c2bd70b15dde4d372671d0
Cristobal Andres Manzano Martinez <zano@live.cl>
2023-11-21 12:54:24 +0700

    update

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,64 +1,110 @@
-import { Button, TextField } from '@mui/material'
+import { Box, Button, TextField } from '@mui/material'
 import React, { useState } from 'react'
+import Swal from 'sweetalert2';
+import lemonSvg from '/src/assets/lemon.svg';
 
 export const SimpleForm = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
+    const [isSent, setIsSent] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log({
-            firstName,
-            lastName,
-            email,
-            phoneNumber,
+        console.log('Form submitted:', { firstName, lastName, email, phoneNumber });
+        setIsSent(true);
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Message Sent successfully",
+            showConfirmButton: false,
+            timer: 1500
         });
+
+        setTimeout(() => {
+            setIsSent(false);
+            setFirstName('');
+            setLastName('');
+            setEmail('');
+            setPhoneNumber('');
+        }, 5000);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        const trimmedValue = value.trim();
+
+        if (trimmedValue) {
+            if (name === 'firstName') {
+                setFirstName(trimmedValue);
+            } else if (name === 'lastName') {
+                setLastName(trimmedValue);
+            } else if (name === 'email') {
+                setEmail(trimmedValue);
+            } else if (name === 'phoneNumber') {
+                setPhoneNumber(trimmedValue);
+            }
+        }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField
-                label="First Name"
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-            />
-            <TextField
-                label="Last Name"
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-            />
-            <TextField
-                label="Email"
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-                label="Phone Number"
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-            <Button fullWidth size='large' variant="contained" color="primary" type="submit" sx={{ marginTop: 3 }}>
-                Send your message
-            </Button>
-        </form>
+        <>
+            {!isSent ? (
+
+                <form onSubmit={handleSubmit} >
+                    <TextField
+                        label="First Name"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="firstName"
+                        value={firstName}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Last Name"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="lastName"
+                        value={lastName}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Email"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Phone Number"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="phoneNumber"
+                        value={phoneNumber}
+                        onChange={handleChange}
+                    />
+                    <Button fullWidth size="large" variant="contained" color="primary" type="submit" sx={{ marginTop: 3 }}>
+                        Send your message
+                    </Button>
+                </form>
+            ) :
+                (
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <img src={lemonSvg} alt="Lemon Icon" width="100" />
+                    </Box>
+                )
+            }
+        </>
     )
 }
--- src/routes/ContactUs.jsx
+++ src/routes/ContactUs.jsx
@@ -4,7 +4,13 @@ import { SimpleForm } from '../components/SimpleForm'
 
 export const ContactUs = () => {
     return (
-        <Box minHeight={'70vh'} margin={4} justifyContent={'center'} width={'30%'} marginX={'auto'}>
+        <Box
+            minHeight={'70vh'}
+            margin={4}
+            justifyContent={'center'}
+            sx={{ width: { xs: '100%', sm: '50%', md: '40%' } }}
+            marginX={'auto'}
+        >
             <Paper sx={{ padding: 8, display: 'flex', flexDirection: { xs: 'column', md: 'column' }, gap: 4 }}>
                 <Typography variant='h2' textAlign={'center'}>Contact Us</Typography>
                 <SimpleForm />
